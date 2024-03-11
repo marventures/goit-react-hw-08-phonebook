cab5869b3ab8956f3ee2fc8e9a7caa56e259b838
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2024-03-11 19:15:07 +0800

    Update code

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,27 +1,21 @@
-const styles = {
-  container: {
-    minHeight: 'calc(100vh - 50px)',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontWeight: 500,
-    fontSize: 48,
-    textAlign: 'center',
-  },
-};
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import css from './HomePage.module.css';
 
 const HomePage = () => {
   return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>
-        Phonebook welcome page{' '}
-        <span role="img" aria-label="Greeting icon">
-          💁‍♀️
-        </span>
-      </h1>
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <title>Phonebook</title>
+      </Helmet>
+      <div className={css.container}>
+        <h1 className={css.title}>
+          Phonebook welcome page{' '}
+          <span role="img" aria-label="Greeting icon">
+            💁‍♀️
+          </span>
+        </h1>
+      </div>
+    </HelmetProvider>
   );
 };
 
